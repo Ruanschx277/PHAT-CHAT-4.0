250d1693c0d0ac0acf800c0f6845bb97418b7a8f
Ruan Schoeman  231352 <125902884+Ruanschx277@users.noreply.github.com>
2023-10-14 15:26:43 +0200

    231352

--- JS/Sign In.js
+++ JS/Sign In.js
@@ -1,18 +1,35 @@
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+
+function handleFormSubmit(event) {
+    event.preventDefault(); 
+ 
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+ 
+    fetch("authenticate.php", {
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.authenticated) {
+               
+                window.location.href = "/Page/HOME.HTML"; 
+            } else {
+               
+                alert("Authentication failed. Please check your username and password.");
+            }
+        })
+        .catch(error => {
+            console.error("Error: ", error);
+        });
+}
 
 
-
-    if ($username === 'your_username' && $password === 'your_password') {
-       
-        session_start();
-        $_SESSION['authenticated'] = true;
-        header('Location: /Page/HOME.HTML'); 
-        exit();
-    } else {
-       
-        header('Location: /sign-in.html'); 
-        exit();
-    }
+const signinForm = document.getElementById("signin-form");
+if (signinForm) {
+    signinForm.addEventListener("submit", handleFormSubmit);
 }
