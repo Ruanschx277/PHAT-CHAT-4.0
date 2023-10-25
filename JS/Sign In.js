19615a49a3599c67b6ccc5b92d484dcd71a74032
Ruan Schoeman  231352 <125902884+Ruanschx277@users.noreply.github.com>
2023-10-25 13:13:08 +0200

    231352

--- JS/Sign In.js
+++ JS/Sign In.js
@@ -1,76 +1,40 @@
+/*Back to top button*/
+window.onscroll = function() {
+    scrollFunction();
+  };
 
-function handleFormSubmit(event) {
-    event.preventDefault(); 
- 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-
- 
-    fetch("authenticate.php", {
-        method: "POST",
-        body: JSON.stringify({ username, password }),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.authenticated) {
-               
-                window.location.href = "/Page/HOME.HTML"; 
-            } else {
-               
-                alert("Authentication failed. Please check your username and password.");
-            }
-        })
-        .catch(error => {
-            console.error("Error: ", error);
-        });
-}
-
-
-const signinForm = document.getElementById("signin-form");
-if (signinForm) {
-    signinForm.addEventListener("submit", handleFormSubmit);
-}
-
-
-
-
-function validateForm() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-
-
-    if (username === "" || password === "") {
-        alert("Username and password are required");
-        return false;
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("back-to-top-button").style.display = "block";
+    } else {
+      document.getElementById("back-to-top-button").style.display = "none";
     }
+  }
 
- 
-    var userData = {
-        username: username,
-        password: password
-    };
-    localStorage.setItem('userData', JSON.stringify(userData));
+  function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  }
 
+/*Add to Watch List button*/  
+// Sample watch later list (you should use a server or local storage for a real application)
+let watchLaterList = [];
 
-    window.location.href = "/Page/HOME.HTML"; 
-
-    return false; 
+// Function to add a movie to the watch later list
+function addToWatchLater(movieId) {
+  if (!watchLaterList.includes(movieId)) {
+    watchLaterList.push(movieId);
+    alert('Movie added to Watch Later list');
+    // You can also send the updated watch later list to the server here
+  } else {
+    alert('Movie is already in the Watch Later list');
+  }
 }
 
-const apiUrl = '';
-
-$.ajax({
-    url: apiUrl,
-    method: 'GET',
-    dataType: 'json',
-    success: function(data) {
-     
-        console.log(data);
-    },
-    error: function(error) {
-        console.error('Error:', error);
-    }
+// Function to handle the "Watch Later" button click
+document.querySelectorAll('.watch-later-button').forEach(button => {
+  button.addEventListener('click', function() {
+    const movieId = this.getAttribute('data-movie-id');
+    addToWatchLater(movieId);
+  });
 });
