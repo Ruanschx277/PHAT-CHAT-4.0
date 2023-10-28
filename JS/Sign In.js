31a677e39d0527f5e2eba88b8f5aaa22d005837c
Ruan Schoeman  231352 <125902884+Ruanschx277@users.noreply.github.com>
2023-10-28 16:46:15 +0200

    23

--- JS/Sign In.js
+++ JS/Sign In.js
@@ -1,76 +1,52 @@
+document.getElementById("login-button").addEventListener("click", function () {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-function handleFormSubmit(event) {
-    event.preventDefault(); 
- 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-
- 
-    fetch("authenticate.php", {
-        method: "POST",
-        body: JSON.stringify({ username, password }),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.authenticated) {
-               
-                window.location.href = "/Page/HOME.HTML"; 
-            } else {
-               
-                alert("Authentication failed. Please check your username and password.");
-            }
-        })
-        .catch(error => {
-            console.error("Error: ", error);
-        });
-}
-
-
-const signinForm = document.getElementById("signin-form");
-if (signinForm) {
-    signinForm.addEventListener("submit", handleFormSubmit);
-}
-
-
-
-
-function validateForm() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-
-
-    if (username === "" || password === "") {
-        alert("Username and password are required");
-        return false;
+    if (username === "your_username" && password === "your_password") {
+      window.location.href = "/home.html"; // Redirect to the home page if login is successful
+    } else {
+      alert("Invalid username or password. Please try again.");
     }
 
- 
-    var userData = {
-        username: username,
-        password: password
-    };
-    localStorage.setItem('userData', JSON.stringify(userData));
+
+    document.getElementById("forgot-password-link").addEventListener("click", function () {
+        // Prompt the user to enter their email address for password reset
+        var userEmail = prompt("Enter your email address for password reset:");
+      
+        // Check if the user entered an email address
+        if (userEmail !== null) {
+          // Send the email address to the server for password reset
+          // You can implement this logic based on your backend and email service
+          // Here, we'll just log the email for demonstration purposes
+          console.log("Password reset email sent to: " + userEmail);
+          alert("Password reset instructions have been sent to your email.");
+        }
+      });
 
 
-    window.location.href = "/Page/HOME.HTML"; 
+      document.addEventListener("DOMContentLoaded", function() {
+        // Check if the user is signed in
+        const userIsSignedIn = /* Replace this with your logic to check if the user is signed in */;
+    
+        if (userIsSignedIn) {
+            // User is signed in, redirect to the home page
+            window.location.href = "/Page/HOME.HTML"; // Replace with the actual URL of your home page
+        }
+    });
 
-    return false; 
-}
 
-const apiUrl = '';
-
-$.ajax({
-    url: apiUrl,
-    method: 'GET',
-    dataType: 'json',
-    success: function(data) {
-     
+    // Your AJAX request
+    $.ajax({
+      url: 'your_api_url_here', // Replace with the actual API URL
+      method: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        // Handle the response from the server
         console.log(data);
-    },
-    error: function(error) {
+      },
+      error: function(error) {
+        // Handle AJAX request errors
         console.error('Error:', error);
-    }
-});
+      }
+    });
+  });
--- JS/signup.js
+++ JS/signup.js
@@ -1,48 +1,42 @@
-const signupButton = document.getElementById("signup-button");
-const signinButton = document.getElementById("signin-button");
+document.addEventListener("DOMContentLoaded", function () {
+    const signupForm = document.querySelector("form");
+    const errorMessage = document.querySelector("#error-message");
 
-signupButton.addEventListener("click", function() {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    if (signupForm) {
+        signupForm.addEventListener("submit", function (event) {
+            event.preventDefault();
 
-    if (localStorage.getItem(username) !== null) {
-        alert("Username already exists. Please choose a different one.");
-        return;
+            // Get form values
+            const username = signupForm.querySelector('input[name="Username"]').value;
+            const email = signupForm.querySelector('input[name="email"]').value;
+            const password = signupForm.querySelector('input[name="password"]').value;
+            const confirmPassword = signupForm.querySelector('input[name="confirmPassword"]').value;
+
+            // Check if passwords match
+            if (password !== confirmPassword) {
+                // Display an error message
+                errorMessage.textContent = "Passwords do not match!";
+            } else {
+                // Clear the error message
+                errorMessage.textContent = "";
+
+                // Create a user object with the form data
+                const user = {
+                    username: username,
+                    email: email,
+                    password: password,
+                };
+
+                // Store the user object in local storage
+                localStorage.setItem("user", JSON.stringify(user));
+
+                // Redirect to the sign-in page
+                window.location.href = "/Page/Sign In.html"; // Update the URL as needed
+            }
+        });
     }
-
-    localStorage.setItem(username, password);
-    alert("Sign-up successful! You can now sign in.");
-
-    window.location.href = "/Page/Sign In.html";
 });
 
-signinButton.addEventListener("click", function() {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-
-    const storedPassword = localStorage.getItem(username);
-
-    if (storedPassword === password) {
-        alert("Sign-in successful!");
-    } else {
-        alert("Invalid username or password. Please try again.");
-    }
-});
-
-function validatePassword() {
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirm-password").value;
-    var passwordError = document.getElementById("password-error");
-
-    if (password !== confirmPassword) {
-        passwordError.textContent = "Password and Confirm Password do not match.";
-        return false;
-    } else {
-        passwordError.textContent = "Passwords match!";
-    }
-
-    return true;
-}
 
 
 
