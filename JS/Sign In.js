1114fb97d294de4e1125cbbc58677ca78973f315
Ruan Schoeman  231352 <125902884+Ruanschx277@users.noreply.github.com>
2023-10-31 12:23:17 +0200

    23

--- JS/Sign In.js
+++ JS/Sign In.js
@@ -1,20 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const signInForm = document.querySelector(".signin-form");
+    const signInForm = document.querySelector("Signin-form");
     const usernameInput = document.querySelector("input[placeholder='Username']");
     const passwordInput = document.querySelector("input[placeholder='Password']");
-    const forgotPasswordLink = document.getElementById("forgot-password-link");
+    const forgotPasswordLink = document.getElementById("forgot-passwordreset-link");
 
     signInForm.addEventListener("submit", function (event) {
         event.preventDefault();
         const username = usernameInput.value;
         const password = passwordInput.value;
 
-        // Retrieve user data from local storage
-        const storedUserData = JSON.parse(localStorage.getItem(username));
+        // In a real application, you should send the username and password to the server for authentication.
+        // This is a basic example for demonstration purposes.
+        const users = {
+            "your_username": "your_password", // Replace with actual user data
+        };
 
-        if (storedUserData && storedUserData.password === password) {
+        if (users[username] === password) {
             // Redirect to the home page after successful login
-            window.location.href = "/Page/home.html";
+            window.location.href = "/Page/HOME.HTML";
         } else {
             alert("Invalid username or password. Please try again.");
         }
@@ -24,9 +27,16 @@ document.addEventListener("DOMContentLoaded", function () {
         event.preventDefault();
         var userEmail = prompt("Enter your email address for password reset:");
         if (userEmail !== null) {
+            // In a real application, you would send a password reset email to the provided email address.
             console.log("Password reset email sent to: " + userEmail);
             alert("Password reset instructions have been sent to your email.");
         }
     });
 });
+
+
+
+
+  
+
 
--- JS/passwordreset.js
+++ JS/passwordreset.js
@@ -1,16 +1,21 @@
-
 document.addEventListener("DOMContentLoaded", function() {
-  
     const resetPasswordForm = document.getElementById("reset-password-form");
-
+    const forgotPasswordLink = document.getElementById("forgot-password-link");
 
     resetPasswordForm.addEventListener("submit", function(event) {
-        event.preventDefault(); 
-
+        event.preventDefault(); // Prevent the default form submission behavior
 
         const email = document.getElementById("email").value;
 
-
         alert(`A password reset link will be sent to ${email}.`);
+    });
+
+    // Add an event listener for the "Forgot Password" link
+    forgotPasswordLink.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent the default link behavior
+
+       
+        // For example, you can use window.location.href to redirect to the "Forgot Password" page.
+        window.location.href = "/Page/Password reset.html";
     });
 });
