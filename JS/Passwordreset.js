00912b649991b2201b62b2a4ba86ac8b47e878fd
TazzLeigh <125860291+TazzLeigh@users.noreply.github.com>
2023-11-06 13:41:25 +0200

    231023

--- JS/Passwordreset.js
+++ JS/Passwordreset.js
@@ -1,17 +1,21 @@
-// Wait for the document to be ready
 document.addEventListener("DOMContentLoaded", function() {
-    // Find the form element
     const resetPasswordForm = document.getElementById("reset-password-form");
+    const forgotPasswordLink = document.getElementById("forgot-password-link");
 
-    // Add a submit event listener to the form
     resetPasswordForm.addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevent the default form submission
+        event.preventDefault(); // Prevent the default form submission behavior
 
-        // Get the email input value
         const email = document.getElementById("email").value;
 
-        // Here, you can implement logic for sending a reset email to the provided email address.
-        // For this example, we will simply display a message.
         alert(`A password reset link will be sent to ${email}.`);
+    });
+
+    // Add an event listener for the "Forgot Password" link
+    forgotPasswordLink.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent the default link behavior
+
+       
+        // For example, you can use window.location.href to redirect to the "Forgot Password" page.
+        window.location.href = "/Page/Password reset.html";
     });
 });
